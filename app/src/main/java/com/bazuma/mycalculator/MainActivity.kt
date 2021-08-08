91c332130bcc411b68317d10101d51bbc3c0d85c
AyubuAyubu <ayubu6143@gmail.com>
2021-08-08 08:25:05 +0300

    Second Experimental Commit

--- app/src/main/java/com/bazuma/mycalculator/MainActivity.kt
+++ app/src/main/java/com/bazuma/mycalculator/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println("Commit 1")
         println("Android is awesome")
     }
     //step 6
